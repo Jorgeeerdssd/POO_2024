192a8928ed0ea3045fe2c98aef6476b6c6d880f9
Jorgeeerdssd <150379015+Jorgeeerdssd@users.noreply.github.com>
2024-05-21 10:21:18 -0600

    aa

--- Parcial1/6_ejercicios_repaso/ejercicio5.py
+++ Parcial1/6_ejercicios_repaso/ejercicio5.py
@@ -1,7 +1,11 @@
 #Hacer un programa que muestre todos los numeros entre 2 numeros que diga el usuario
-lln2=input("Ingresa un numero")
+n1=int(input("Ingresa un numero"))
+n2=int(input("Ingresa un numero"))
 
-for i in range (n1,n2):
-    n1=int
-    n2=int
-    print (i)
+
+if n1<n2:
+    for i in range (n1,n2):
+        print (i)
+elif n1 > n2:
+    for i in range (n2, n1):
+        print(i)
